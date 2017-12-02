3c004e13e08bacc8a285c7a11e63726724577060
AdamMc331 <amcneilly331@gmail.com>
2017-12-01 23:11:47 -0500

    Updated tests and documentation on part two method.

--- src/test/java/dayone/DayOneTest.kt
+++ src/test/java/dayone/DayOneTest.kt
@@ -7,19 +7,43 @@ class DayOneTest {
     private val dayOne = DayOne()
 
     @Test
-    fun testPartOne() {
+    fun testPartOneTwoMatches() {
         assertEquals(3, dayOne.partOne("1122"))
+    }
+
+    @Test
+    fun testPartOneAllMatchWithWrap() {
         assertEquals(4, dayOne.partOne("1111"))
+    }
+
+    @Test
+    fun testPartOneNoMatches() {
         assertEquals(0, dayOne.partOne("1234"))
+    }
+
+    @Test
+    fun testPartOneWithWrap() {
         assertEquals(9, dayOne.partOne("91212129"))
     }
 
     @Test
-    fun testPartTwo() {
+    fun testPartTwoAllMatches() {
         assertEquals(6, dayOne.partTwo("1212"))
-        assertEquals(0, dayOne.partTwo("1221"))
-        assertEquals(4, dayOne.partTwo("123425"))
         assertEquals(12, dayOne.partTwo("123123"))
+    }
+
+    @Test
+    fun testPartTwoNoMatches() {
+        assertEquals(0, dayOne.partTwo("1221"))
+    }
+
+    @Test
+    fun testPartTwoOneMatch() {
+        assertEquals(4, dayOne.partTwo("123425"))
+    }
+
+    @Test
+    fun testPartTwoAlternatingMatches() {
         assertEquals(4, dayOne.partTwo("12131415"))
     }
 }
